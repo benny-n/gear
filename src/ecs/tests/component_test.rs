d97345bced93cd63185159c9e2f51097a1e09617
benny-n <benick12@gmail.com>
2021-08-01 18:33:05 +0300

    added removal API, added removal tests
    made API to get entity manager instance a bit cleaner.
    refactored ComponentTypeId to standard library's TypeId which already implements unique type identifiers logic.
    tests work concurrently now.

--- src/ecs/tests/component_test.rs
+++ src/ecs/tests/component_test.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use std::any::Any;
+    use std::any::{Any, type_name};
     use crate::math_engine::Vector3;
-    use crate::ecs::Component;
-    use crate::ecs::entity_manager::ENTITY_MANAGER;
+    use crate::ecs::{Component, entity_manager};
+    
 
     #[derive(Default, Component)]
     struct Shape {
@@ -17,26 +17,8 @@ mod tests {
     }
 
     #[test]
-    fn get_id_api_test(){
-        let entity_manager = &mut *ENTITY_MANAGER.lock().unwrap();
-        let id1 = entity_manager.get_component_type_id::<Transform>();
-        let id2 = entity_manager.get_component_type_id::<Shape>();
-        let id3 = entity_manager.get_component_type_id::<Transform>();
-        let id4 = entity_manager.get_component_type_id::<Transform>();
-        let id5 = entity_manager.get_component_type_id::<Shape>();
-        
-        assert_eq!(id1, id3);
-        assert_ne!(id1, id2);
-        assert_ne!(id3, id5);
-        assert_ne!(id5, id4);
-        assert_eq!(id5, id2);
-        assert_eq!(id3, id4);
-        entity_manager.clear();
-    }
-
-    #[test]
     fn component_modify_test() -> Result<(), String>{
-        let entity_manager = &mut *ENTITY_MANAGER.lock().unwrap();
+        let entity_manager = &mut *entity_manager::get_instance();
         let ent1 = entity_manager.create_entity();
         let ent2 = entity_manager.create_entity();
         
@@ -61,7 +43,7 @@ mod tests {
 
     #[test]
     fn component_assign_test(){
-        let entity_manager = &mut *ENTITY_MANAGER.lock().unwrap();
+        let entity_manager = &mut *entity_manager::get_instance();
         let ent1 = entity_manager.create_entity();
         let ent2 = entity_manager.create_entity();
         let ent3 = entity_manager.create_entity();
@@ -73,18 +55,18 @@ mod tests {
 
         match entity_manager.get_component::<Transform>(ent2){
             Ok(_) => (),
-            Err(err_msg) => panic!("{}", err_msg)
+            Err(err_msg) => unreachable!("{}", err_msg)
         }
         match entity_manager.get_component::<Transform>(ent3){
             Ok(_) => (),
-            Err(err_msg) => panic!("{}", err_msg)
+            Err(err_msg) => unreachable!("{}", err_msg)
         }
         match entity_manager.get_component::<Shape>(ent3){
             Ok(_) => (),
-            Err(err_msg) => panic!("{}", err_msg)
+            Err(err_msg) => unreachable!("{}", err_msg)
         }
         match entity_manager.get_component::<Shape>(ent1){
-            Ok(_) => panic!("ent1 shouldn't have \"Shape\" component"),
+            Ok(_) => unreachable!("ent1 shouldn't have \"Shape\" component"),
             Err(_) => ()
         }
         entity_manager.clear();
@@ -92,20 +74,20 @@ mod tests {
 
     #[test]
     fn component_pool_size_test(){
-        let entity_manager = &mut *ENTITY_MANAGER.lock().unwrap();
+        let entity_manager = &mut *entity_manager::get_instance();
         let ent1 = entity_manager.create_entity();
         let ent2 = entity_manager.create_entity();
         let ent3 = entity_manager.create_entity();
 
         match entity_manager.get_component_pool_size::<Transform>(){
-            Ok(_) => panic!("Should be empty!"),
+            Ok(_) => unreachable!("Should be empty!"),
             Err(_) => ()
         };
 
         entity_manager.assign::<Transform>(ent1).expect("0");
         let pool_size = match entity_manager.get_component_pool_size::<Transform>(){
             Ok(size) => size,
-            Err(err_msg) => panic!("{}", err_msg),
+            Err(err_msg) => unreachable!("{}", err_msg),
         };
 
         assert_eq!(pool_size, 1);
@@ -115,7 +97,7 @@ mod tests {
 
         let pool_size = match entity_manager.get_component_pool_size::<Transform>(){
             Ok(size) => size,
-            Err(err_msg) => panic!("{}", err_msg),
+            Err(err_msg) => unreachable!("{}", err_msg),
         };
 
         assert_eq!(pool_size, 3);
@@ -123,12 +105,52 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "not yet implemented")]
     fn remove_component_test(){
-        let entity_manager = &mut *ENTITY_MANAGER.lock().unwrap();
+        let entity_manager = &mut *entity_manager::get_instance();
         let ent1 = entity_manager.create_entity();
-        entity_manager.assign::<Transform>(ent1).expect("0");
-        entity_manager.remove::<Transform>(ent1).unwrap(); // Should panic here
+        let ent2 = entity_manager.create_entity();
+        
+        entity_manager.assign::<Transform>(ent1).unwrap();
+        entity_manager.assign::<Transform>(ent2).unwrap();
+        let result = entity_manager.get_component::<Transform>(ent1);
+        match result{
+            Ok(_) => (),
+            Err(err_msg) => unreachable!("{}", err_msg),
+        }
+        
+        entity_manager.remove::<Transform>(ent1).unwrap();
+        let result = entity_manager.get_component::<Transform>(ent1);
+        match result{
+            Ok(_) => unreachable!(), 
+            Err(err_msg) => assert_eq!(err_msg, String::from("The entity with ID 0 does not own this component!"))
+        }
+        entity_manager.clear();
+    }
+
+    #[test]
+    fn remove_entity_test(){
+        let entity_manager = &mut *entity_manager::get_instance();
+        let ent1 = entity_manager.create_entity();
+        
+        entity_manager.assign::<Transform>(ent1).unwrap();
+        let result = entity_manager.get_component::<Transform>(ent1);
+        match result{
+            Ok(_) => (),
+            Err(err_msg) => unreachable!("{}", err_msg),
+        }
+        
+        entity_manager.remove_entity(ent1).unwrap();
+        let result = entity_manager.get_component::<Transform>(ent1);
+        match result{
+            Ok(_) => unreachable!(), 
+            Err(err_msg) => assert_eq!(err_msg, format!("The component {} does not exist in this scene!", type_name::<Transform>()))
+        }
+
+        let result = entity_manager.remove_entity(ent1);
+        match result{
+            Ok(_) => unreachable!(), 
+            Err(err_msg) => assert_eq!(err_msg, String::from("The entity with ID 0 does not exist!"))
+        }
         entity_manager.clear();
     }
 }
